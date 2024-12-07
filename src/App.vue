6c1afc95ae2d8e4ddb8ddffc46a41dc22cbf3de1
太田章吾 <ootashougo@ootashougonoMacBook-Air.local>
2024-12-07 11:27:16 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <!-- ★Boardコンポーネント呼び出し -->
+    <board />
+  </div>
+</template>
+
+<script>
+// ★インポート文を追加
+import Board from './components/Board.vue'
+
+export default {
+  name: 'app',
+  components: {
+    // ★コンポーネントに登録する
+    Board
+  }
+}
+</script>
+
